b8084fd7e15afc894fd41438466dc8adbb54d8ff
Marinela Rajkovic <marinelic555@gmail.com>
2025-09-19 16:29:14 +0200

    Adding HTML and CSS

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,51 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="game">
+        <h1>Tic Tac Toe</h1>
+          <div id="board">
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+              <button className="cell">
+                  <span className="cell-content"></span>
+              </button>
+          </div>
+  </div>
+
+  <div id="modal-overlay">
+      <div id="game-result-modal">
+          <div id="result-container">
+              <div id="winner-container">
+                  <span></span>
+              </div>
+          </div>
+          <div id="new-game-container">
+              <button id="new-game-button">Start New Game</button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  </div>
     </>
   )
 }
